98f7ea9fd7fe261564bbf945bfda773866e3ee09
unknown <dominik.spiljak@gmail.com>
2021-01-03 17:30:33 +0100

    Added Runge-KutTa predictor

--- lab5/predictors.py
+++ lab5/predictors.py
@@ -35,16 +35,13 @@ class EulerPredictor(Predictor):
     def predict(self, linear_system, num_iter_print=None):
         predicted_values = [linear_system.initial_state]
 
-        derivation_value = linear_system.calculate(
-            linear_system.initial_state, 0)
-
         t = self.integration_period
         i = 0
 
         while t <= self.max_time:
 
             predicted_value = self.calculate_step(
-                predicted_values, derivation_value)
+                predicted_values, t, linear_system)
             predicted_values.append(predicted_value)
 
             if num_iter_print is not None:
@@ -52,16 +49,16 @@ class EulerPredictor(Predictor):
                     print('Euler prediction:\nSystem state t={}: \nx={}'.format(
                         t, predicted_value))
 
-            derivation_value = linear_system.calculate(predicted_value, t)
-
             t = round(t + self.integration_period, 8)
             i += 1
 
-        print('System state t={}: \nx={}'.format(
+        print('Euler prediction:\nSystem state t={}: \nx={}'.format(
             self.max_time, predicted_value))
         return np.array(predicted_values)
 
-    def calculate_step(self, predicted_values, derivation_value):
+    def calculate_step(self, predicted_values, t, linear_system):
+        derivation_value = linear_system.calculate(predicted_values[-1], t - self.integration_period)
+
         delta_x = self.integration_period * derivation_value
         return predicted_values[-1] + delta_x
 
@@ -140,10 +137,43 @@ class TrapezoidalPredictor(Predictor):
         return R.dot(predicted_values[-1]) + S.dot(external_stimulus(t - self.integration_period) + external_stimulus(t))
 
 
+class RungeKutta4Predictor(Predictor):
+
+    def predict(self, linear_system, num_iter_print=None):
+        predicted_values = [linear_system.initial_state]
+
+        t = self.integration_period
+        i = 0
+
+        while t <= self.max_time:
+
+            predicted_value = self.calculate_step( predicted_values, t, linear_system)
+            predicted_values.append(predicted_value)
+
+            if num_iter_print is not None:
+                if (i + 1) % num_iter_print == 0:
+                    print('RungeKutta prediction:\nSystem state t={}: \nx={}'.format(
+                        t, predicted_value))
+
+            t = round(t + self.integration_period, 8)
+            i += 1
+
+        print('RungeKutta prediction:\nSystem state t={}: \nx={}'.format(
+            self.max_time, predicted_value))
+        return np.array(predicted_values)
+
+    def calculate_step(self, predicted_values, t, linear_system):
+        tk = t - self.integration_period
+        m1 = linear_system.calculate(predicted_values[-1], tk)
+        m2 = linear_system.calculate(predicted_values[-1] + self.integration_period / 2 * m1, tk + self.integration_period / 2)
+        m3 = linear_system.calculate(predicted_values[-1] + self.integration_period / 2 * m2, tk + self.integration_period / 2)
+        m4 = linear_system.calculate(predicted_values[-1] + self.integration_period * m3, tk + self.integration_period)
+        return predicted_values[-1] + self.integration_period / 6 * (m1 + 2 * m2 + 2 * m3 + m4)
+
 def main():
     ls = LinearSystem(np.array([[0, 1], [-200, -102]]), np.array([[1], [-2]]))
 
-    s_e = (0.01, 0.02)
+    s_e = (0.01, 0.1)
 
     ep = EulerPredictor(*s_e)
     ep.predict(ls)
@@ -154,6 +184,8 @@ def main():
     tp = TrapezoidalPredictor(*s_e)
     tp.predict(ls)
 
+    rk = RungeKutta4Predictor(*s_e)
+    rk.predict(ls)
 
 if __name__ == "__main__":
     main()
